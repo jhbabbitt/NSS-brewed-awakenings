423e2b942e61f16c80bced50417e3a2104aa01c1
Jacob Babbitt <jacobhbabbitt@gmail.com>
2022-12-12 15:46:22 -0600

    working click events

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,6 +1,29 @@
 import { getEmployees } from "./database.js"
+import { Orders } from "./Orders.js"
 
 const employees = getEmployees()
+const orders = Orders()
+
+const employeeOrders = (employee) => {
+    let fullfilledOrders = 0
+    for (const order of Orders) 
+}
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [, employeeId] = itemClicked.id.split("--")
+
+            for (const employee of employees) {
+                if (product.id === parseInt(employeeId)) {
+                    window.alert(` ${employee.name} sold ${product.price} `)
+                }
+            }
+        }
+    }
+)
 
 export const Employees = () => {
     let html = "<ul>"
